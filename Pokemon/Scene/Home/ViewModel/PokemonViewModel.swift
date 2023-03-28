ed3ff5779b45e2961cb753b4b7b48d1ffde20536
baris dilekci <bdilekci200@gmail.com>
2023-03-28 15:54:42 +0300

    The basic pokemon details viewModel has been designed.

--- Pokemon/Scene/Home/ViewModel/PokemonViewModel.swift
+++ Pokemon/Scene/Home/ViewModel/PokemonViewModel.swift
@@ -9,21 +9,35 @@ import Foundation
 
 protocol PokemonViewModelProtocol {
     func getPokemon(completion: @escaping ((String?) ->Void))
+    func getPokemonSpritesURL(index:Int) -> URL?
+    
 }
 final class PokemonViewModel: PokemonViewModelProtocol {
     let manager = PokemonManager.shared
-    var pokemons : GetPokemonsResponse?
+    var pokemons: [GetPokemonsResponse.Pokemon] = []
 }
 
 extension PokemonViewModel {
     func getPokemon(completion: @escaping ((String?) -> Void)) {
-        manager.fetchPokemon { pokemons, error in
+        manager.fetchPokemons { pokemons, error in
             if pokemons != nil {
-                self.pokemons = pokemons
+                self.pokemons = pokemons?.results ?? []
             }
             completion(error?.localizedDescription)
         }
     }
+    
+    func getPokemonSpritesURL(index: Int) -> URL? {
+        let pokemon = pokemons[index]
+        return PokemonManager.shared.fetchPokemonSprites(id: pokemon.pokemonID)!
+    }
+    func getPokemonDetailURL(for index: Int) -> URL {
+        let pokemon = pokemons[index]
+        return PokemonManager.shared.getPokemonOfficialArtwork(id: pokemon.pokemonID)!
+    }
+    
+    
 }
+
 
 
